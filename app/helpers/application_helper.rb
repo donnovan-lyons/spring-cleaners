0fde0f10170d107227dc9248b41dbc32752dbf45
dlyons8 <dlyons8@binghamton.edu>
2020-04-05 22:49:54 +0300

    updated Gemfile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-  def logged_in?
-    !!session[:user_id]
-  end
+  # def logged_in?
+  #   !!session[:user_id]
+  # end
 
-  def current_user
-    @current_user ||= User.find(session[:user_id])
-  end
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id])
+  # end
 
-  def authorized?(id)
-    current_user.id == id
-  end
+  # def authorized?(id)
+  #   current_user.id == id
+  # end
 end
