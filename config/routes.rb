aab958ad7c0b28b428c7c811cc63bb538443dc4a
dlyons8 <dlyons8@binghamton.edu>
2020-04-13 20:51:59 +0300

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :appointments
-  resources :customers
-  resources :cleaners
-  resources :sessions
   root to: 'welcome#homepage'
   get 'login', to: 'sessions#new'
   get 'signup', to: 'users#new'
-  # post 'login', to: 'sessions#create'
+  post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
+  # get 'cleaners/:cleaner_id/appointments/new', to: 'appointments#cleaner_new'
+  # get 'cleaners/:cleaner_id/appointments', to: 'appointments#cleaner_index'
+  # get 'cleaners/:cleaner_id/appointments/:id/edit', to: 'appointments#cleaner_edit'
+  resources :users, only: [:index, :new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :cleaners, only: [:new, :create, :show] 
+  resources :customers, only: [:new, :create, :show] do
+    resources :appointments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
