422c26405b71d53b875c2caea9790109fbe8f94f
dlyons8 <dlyons8@binghamton.edu>
2020-04-19 23:30:30 +0300

    removed comment

--- app/models/cleaner.rb
+++ app/models/cleaner.rb
@@ -1,7 +1,6 @@
 class Cleaner < ApplicationRecord
     validates :first_name, :last_name, :phone,  presence: true
     validates :first_name, :last_name, length: { minimum: 2 }
-    # validates address: [:neighborhood, :city],  presence: true
 
     belongs_to :user
     belongs_to :institution
